4773db3dd419ee510189f83d17d1e96539f54229
Minigm <rush_gm@163.com>
2020-03-15 18:42:42 +0800

    进入设备管理编写
    完成管理主页面编写

---
+++ app/src/main/java/com/gm/smartHomePlatform/Administrator/act_admin_device.java
@@ -0,0 +1,14 @@
+package com.gm.smartHomePlatform.Administrator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.gm.smartHomePlatform.R;
+import android.os.Bundle;
+
+public class act_admin_device extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_admin_device);
+    }
+}
--- app/src/main/java/com/gm/smartHomePlatform/Administrator/act_admin_main.java
+++ app/src/main/java/com/gm/smartHomePlatform/Administrator/act_admin_main.java
@@ -3,22 +3,19 @@ package com.gm.smartHomePlatform.Administrator;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import com.gm.smartHomePlatform.R;
-import com.gm.smartHomePlatform.SQLSeverManeger.UserManager;
 
-import android.content.SharedPreferences;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class act_admin_main extends AppCompatActivity {
-    //子线程进度标志
-    private int CONNECTION_STATE = 0;
-    //子线程处理标志
-    private boolean CONNECTION_FLAG = false;
     //子线程处理消息
-    private final int UNKNOWN_SITUATION = 0,EXIT = 1;
+    private final int UNKNOWN_SITUATION = 0,EXIT = 1,DEVICE_ADMIN = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,39 +36,23 @@ public class act_admin_main extends AppCompatActivity {
     }
     //设备管理按钮响应
     public void buttonDeviceAdmin_admin_mainOnClick(View v){
-        ;
+        mHandler.sendEmptyMessage(DEVICE_ADMIN);
     }
     //退出按钮响应
     public void buttonExit_admin_mainOnClick(View v){
         mHandler.sendEmptyMessage(EXIT);
     }
     //网络处理子线程
-    private class InternetThread extends Thread{
-        @Override
-        public void run(){
-            //获得活动中的数据信息
-            UserManager userManager = new UserManager();
-            SharedPreferences sharedPreferences = getSharedPreferences("signed_information",MODE_PRIVATE);
-            while (CONNECTION_FLAG){
-                switch (CONNECTION_STATE){
-                    //0状态为挂起态，不进行操作
-                    case 0:break;
-                    //1状态为登录处理状态，单次触发仅进入一次后回归0状态
-                    case 1:
-                        CONNECTION_STATE = 0;
-                        break;
-                    default:
-                        //意外运行至此，返回0状态
-                        CONNECTION_STATE = 0;break;
-                }
-            }
-        }
-    }
     //消息处理
     private Handler mHandler = new Handler(){
         public void handleMessage(Message msg){
             if (msg.what == EXIT){
                 finish();
+            }else if (msg.what == DEVICE_ADMIN){
+                startActivity(new Intent(act_admin_main.this,act_admin_device.class));
+            }else if (msg.what == UNKNOWN_SITUATION){
+                Toast to = Toast.makeText(getBaseContext(),"未知错误，请尝试重新操作",Toast.LENGTH_LONG);
+                to.show();
             }
         }
     };
