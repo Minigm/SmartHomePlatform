fb41088cc8de30e3de525f981ee621d25fb25887
Minigm <rush_gm@163.com>
2020-03-17 10:09:11 +0800

    多电脑操作test
    简单修改，尝试两台电脑共同维护

--- app/src/main/java/com/gm/smartHomePlatform/SQLSeverManeger/DeviceManager.java
+++ app/src/main/java/com/gm/smartHomePlatform/SQLSeverManeger/DeviceManager.java
@@ -1,6 +1,9 @@
 package com.gm.smartHomePlatform.SQLSeverManeger;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DeviceManager {
     private Connection connection;
@@ -10,5 +13,28 @@ public class DeviceManager {
         connection = connectManager.getConnection();
         sql = "";
     }
-
+    //用于关闭connection关闭
+    public void disconnected(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //用于内部资源的关闭。
+    private void close(Statement statement, ResultSet resultSet){
+        try {
+            //如果statement有实例，执行close清除。
+            if (statement != null)statement.close();
+            //如果resultSet有实例，执行close清除。
+            if (resultSet != null)resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public int isDevice(String deviceName,String userName){
+        try {
+            sql = "select count(*) where "
+        }
+    }
 }
